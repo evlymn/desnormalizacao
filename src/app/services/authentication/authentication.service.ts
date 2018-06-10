a8636e8ffc4d28909ec19e268000928efa3cf3d7
Evelyn Mendes <jelielmendes@gmail.com>
2018-06-10 20:28:19 -0300

    adicionado imports angularfireauth e firebase

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) {}
 }
