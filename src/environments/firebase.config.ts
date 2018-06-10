6b0d8ac5ac52eb2f602bcbc2b53801b361ad4537
Evelyn Mendes <jelielmendes@gmail.com>
2018-06-10 20:24:54 -0300

    adicionado firebase.config.ts

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyDKG_f8qr-W8zFsZ_j1TFM8rM7laMIyecI',
+  authDomain: 'desnormalizacao.firebaseapp.com',
+  databaseURL: 'https://desnormalizacao.firebaseio.com',
+  projectId: 'desnormalizacao',
+  storageBucket: 'desnormalizacao.appspot.com',
+  messagingSenderId: '731226029508'
+};
