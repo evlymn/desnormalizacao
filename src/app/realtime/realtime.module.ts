179f5cc0c47b8ac2ed058378848cb49c666b26e9
Evelyn Mendes <jelielmendes@gmail.com>
2018-06-11 14:55:57 -0300

    dependencias angular material e imports no realtimemodule

--- src/app/realtime/realtime.module.ts
+++ src/app/realtime/realtime.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RealtimeComponent } from './realtime.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { MatFormControlsModule } from 'src/material_modules/mat-form-controls.module';
+import { FormsModule } from '@angular/forms';
+import { MatNavigationModule } from 'src/material_modules/mat-navigation.module';
+import { MatLayoutModule } from 'src/material_modules/mat-layout.module';
+import { MatButtonsIndicatorsModule } from 'src/material_modules/mat-buttons-indicators.module';
+import { MatPopupsModalsModule } from 'src/material_modules/mat-popups-modals.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatFormControlsModule,
+    MatNavigationModule,
+    MatLayoutModule,
+    MatButtonsIndicatorsModule,
+    MatPopupsModalsModule,
+    FormsModule,
+
+  ],
   declarations: [RealtimeComponent, CadastroComponent],
   exports: [RealtimeComponent]
 })
