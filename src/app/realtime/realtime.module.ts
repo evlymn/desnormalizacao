1101ac7fd7249fe0083af482b9199a12d429f5b9
Evelyn Mendes <jelielmendes@gmail.com>
2018-06-11 14:25:18 -0300

    Add realtime cadastro component

--- src/app/realtime/realtime.module.ts
+++ src/app/realtime/realtime.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RealtimeComponent } from './realtime.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [RealtimeComponent],
+  declarations: [RealtimeComponent, CadastroComponent],
   exports: [RealtimeComponent]
 })
 export class RealtimeModule {}
