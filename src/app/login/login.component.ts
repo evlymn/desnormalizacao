5c39be716d16be0c59deb9fbeb4b729060be7683
Evelyn Mendes <jelielmendes@gmail.com>
2018-06-12 11:51:44 -0300

    atalhos para services e material modulos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../services/authentication/authentication.service';
+import { AuthenticationService } from '@services/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
--- src/app/realtime/realtime.module.ts
+++ src/app/realtime/realtime.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RealtimeComponent } from './realtime.component';
-import { MatFormControlsModule } from 'src/material_modules/mat-form-controls.module';
+import { MatFormControlsModule } from '@material/mat-form-controls.module';
 import { FormsModule } from '@angular/forms';
-import { MatNavigationModule } from 'src/material_modules/mat-navigation.module';
-import { MatLayoutModule } from 'src/material_modules/mat-layout.module';
-import { MatButtonsIndicatorsModule } from 'src/material_modules/mat-buttons-indicators.module';
-import { MatPopupsModalsModule } from 'src/material_modules/mat-popups-modals.module';
+import { MatNavigationModule } from '@material/mat-navigation.module';
+import { MatLayoutModule } from '@material/mat-layout.module';
+import { MatButtonsIndicatorsModule } from '@material/mat-buttons-indicators.module';
+import { MatPopupsModalsModule } from '@material/mat-popups-modals.module';
 import { CartaoModule } from './cartao/cartao.module';
 
 @NgModule({
